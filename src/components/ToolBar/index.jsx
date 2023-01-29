be835633b9e83e3fb7bb9c4bdb9d4f38da51c29e
Marllon Freitas <marllonfreitas64@gmail.com>
2023-01-28 22:41:15 -0300

    feat/adicionando a funcionalidade de drag and drop na criação de nodes

--- src/components/ToolBar/index.jsx
+++ src/components/ToolBar/index.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ToolbarButton, ToolbarRoot } from "./styled";
 
-function ToolBar({ addSquareNode }) {
+function ToolBar() {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
   return (
     <ToolbarRoot>
-      <ToolbarButton onClick={addSquareNode}></ToolbarButton>
+      <ToolbarButton
+        onDragStart={(event) => onDragStart(event, "square")}
+        draggable
+      ></ToolbarButton>
     </ToolbarRoot>
   );
 }
--- src/pages/MainBoard/index.jsx
+++ src/pages/MainBoard/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useRef, useState } from "react";
 import ReactFlow, {
   Controls,
   Background,
@@ -6,6 +6,7 @@ import ReactFlow, {
   addEdge,
   ConnectionMode,
   useNodesState,
+  ReactFlowProvider,
 } from "reactflow";
 import "reactflow/dist/style.css";
 import DefaultEdge from "../../components/Edges/DefaultEdge";
@@ -35,47 +36,92 @@ const INITIAL_NODES = [
     data: {},
   },
 ];
+let id = 0;
+const getId = () => `dndnode_${id++}`;
 
 function MainBoard() {
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
+  const reactFlowWrapper = useRef(null);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
 
   const onConnect = useCallback((connection) => {
     return setEdges((edges) => addEdge(connection, edges));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  function addSquareNode() {
-    const newNode = {
-      id: `${nodes.length + 1}`,
-      type: "square",
-      position: { x: 150, y: 25 },
-      data: {},
-    };
+  // function addSquareNode(mousePosition) {
+  //   console.log("buceta");
+  //   const newNode = {
+  //     id: `${nodes.length + 1}`,
+  //     type: "square",
+  //     position: { x: mousePosition.x, y: mousePosition.y },
+  //     data: {},
+  //   };
 
-    setNodes((nodes) => [...nodes, newNode]);
-  }
+  //   setNodes((nodes) => [...nodes, newNode]);
+  // }
+
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+      const type = event.dataTransfer.getData("application/reactflow");
+
+      // check if the dropped element is valid
+      if (typeof type === "undefined" || !type) {
+        return;
+      }
+
+      const position = reactFlowInstance.project({
+        x: event.clientX - reactFlowBounds.left,
+        y: event.clientY - reactFlowBounds.top,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [reactFlowInstance]
+  );
 
   return (
-    <MainBoardWrapper>
-      <ReactFlow
-        nodeTypes={NODES_TYPES}
-        edgeTypes={EDGE_TYPES}
-        nodes={nodes}
-        edges={edges}
-        connectionMode={ConnectionMode.Loose}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        onNodesChange={onNodesChange}
-        defaultEdgeOptions={{
-          type: "default",
-        }}
-      >
-        <Background variant="dots" gap={12} size={1} color="#d7dce1" />
-        <Controls />
-      </ReactFlow>
-      <ToolBar addSquareNode={addSquareNode} />
-    </MainBoardWrapper>
+    <ReactFlowProvider>
+      <MainBoardWrapper ref={reactFlowWrapper}>
+        <ReactFlow
+          nodeTypes={NODES_TYPES}
+          edgeTypes={EDGE_TYPES}
+          nodes={nodes}
+          edges={edges}
+          connectionMode={ConnectionMode.Loose}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onNodesChange={onNodesChange}
+          onInit={setReactFlowInstance}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          defaultEdgeOptions={{
+            type: "default",
+          }}
+        >
+          <Background variant="dots" gap={12} size={1} color="#d7dce1" />
+          <Controls />
+        </ReactFlow>
+        <ToolBar />
+      </MainBoardWrapper>
+    </ReactFlowProvider>
   );
 }
 
